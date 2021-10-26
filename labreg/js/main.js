a51c1bce237272f7fd34a5fa25e6c57579385553
emil <emilmeldhose@outlook.com>
2021-10-26 11:47:19 +0530

    Added Lab Registration

---
+++ labreg/js/main.js
@@ -0,0 +1,104 @@
+(function($) {
+
+    var form = $("#signup-form");
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) {
+            element.before(error);
+        },
+        rules: {
+            labname: {
+                required: true,
+            },
+            phone: {
+                required: true,
+            },
+            district: {
+                required: true,
+            },
+            lb_address: {
+                required: true,
+            },
+            reg_no: {
+                required: true,
+            },
+          certificate: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email : true
+            }
+            
+        },
+        messages : {
+            email: {
+                email: 'Not a valid email address <i class="zmdi zmdi-info"></i>'
+            }
+        },
+        onfocusout: function(element) {
+            $(element).valid();
+        },
+    });
+    form.steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "slideLeft",
+        labels: {
+            previous: 'Previous',
+            next: 'Next',
+            finish: 'Submit',
+            current: ''
+        },
+        titleTemplate: '<div class="title"><span class="number">#index#</span>#title#</div>',
+        onStepChanging: function(event, currentIndex, newIndex) {
+            form.validate().settings.ignore = ":disabled,:hidden";
+            // console.log(form.steps("getCurrentIndex"));
+            return form.valid();
+        },
+        onFinishing: function(event, currentIndex) {
+            form.validate().settings.ignore = ":disabled";
+            console.log(getCurrentIndex);
+            return form.valid();
+        },
+        onFinished: function(event, currentIndex) {
+            alert('Sumited');
+        },
+        // onInit : function (event, currentIndex) {
+        //     event.append('demo');
+        // }
+    });
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        url: "",
+        date: "",
+        dateISO: "",
+        number: "",
+        digits: "",
+        creditcard: "",
+        equalTo: ""
+    });
+
+
+    $.dobPicker({
+        daySelector: '#expiry_date',
+        monthSelector: '#expiry_month',
+        yearSelector: '#expiry_year',
+        dayDefault: 'DD',
+        yearDefault: 'YYYY',
+        minimumAge: 0,
+        maximumAge: 120
+    });
+
+    $('#password').pwstrength();
+
+    $('#button').click(function () {
+        $("input[type='file']").trigger('click');
+    })
+    
+    $("input[type='file']").change(function () {
+        $('#val').text(this.value.replace(/C:\\fakepath\\/i, ''))
+    })
+
+})(jQuery);
